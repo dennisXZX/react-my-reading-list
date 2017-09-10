a3b5e9535f692e69d3946c52a1da4b2d3e4ab06d
Dennis Xiao <dennisboys@gmail.com>
2017-09-11 08:41:50 +1000

    book adding feature layout update

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
+import BookInput from './book-input';
+import BookAddButton from './book-add-button';
 import BookList from '../containers/book-list'
 import BookDetail from '../containers/book-detail'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <BookList />    
-        <BookDetail />
+      <div className="container">
+	      <div className="row">
+		      <div className="col-xs-12">
+			      <h1 className="text-xs-center">My Reading List</h1>
+		      </div>
+		      <div className="col-xs-12 text-xs-center">
+			      <BookInput />
+			      <BookAddButton />
+		      </div>
+		      <div className="row">
+			      <div className="col-xs-6">
+				      <BookList />
+			      </div>
+			      <div className="col-xs-6">
+				      <BookDetail />
+			      </div>
+		      </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/book-add-button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookAddButton extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<button>Add Book</button>
+		)
+	}
+}
+
+export default BookAddButton;
